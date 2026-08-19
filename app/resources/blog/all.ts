5dcf51416e3e2c234a0290bbab6a811f4ca1a1c0
Jerry emeka <jerryemeka22@gmail.com>
2026-08-19 10:25:37 +0100

    Fix Today’s Harvest catalog exports

--- app/resources/blog/all.ts
+++ app/resources/blog/all.ts
@@ -1,5 +1,9 @@
-import { ALL_HARVEST_ARTICLES, FEATURED_ARTICLE, LATEST_TAX_TALK } from "./catalog";
-import { CURRENT_REFERENCE_ARTICLES, FEATURED_ARTICLE as CURRENT_FEATURED_ARTICLE, LATEST_TAX_TALK as CURRENT_LATEST_TAX_TALK } from "./current";
+import { ALL_HARVEST_ARTICLES } from "./catalog";
+import {
+  CURRENT_REFERENCE_ARTICLES,
+  FEATURED_ARTICLE as LIVE_FEATURED_ARTICLE,
+  LATEST_TAX_TALK as LIVE_LATEST_TAX_TALK,
+} from "./current";
 
 export const ALL_REFERENCE_ARTICLES = [
   ...CURRENT_REFERENCE_ARTICLES,
@@ -9,8 +13,8 @@ export const ALL_REFERENCE_ARTICLES = [
     array.findIndex((item) => item.slug === article.slug) === index,
 );
 
-export const CURRENT_FEATURED_ARTICLE = CURRENT_FEATURED_ARTICLE;
-export const CURRENT_LATEST_TAX_TALK = CURRENT_LATEST_TAX_TALK;
+export const CURRENT_FEATURED_ARTICLE = LIVE_FEATURED_ARTICLE;
+export const CURRENT_LATEST_TAX_TALK = LIVE_LATEST_TAX_TALK;
 
 export function getReferenceArticle(slug: string) {
   return ALL_REFERENCE_ARTICLES.find((article) => article.slug === slug);
