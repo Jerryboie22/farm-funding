bc0d305ef81645883ddc1d26a8c7b117ae867b91
Jerry emeka <jerryemeka22@gmail.com>
2026-08-20 08:35:34 +0100

    Use unified article registry for blog routes

--- app/resources/blog/all.ts
+++ app/resources/blog/all.ts
@@ -1,25 +1,6 @@
-import { ALL_HARVEST_ARTICLES } from "./catalog";
-import {
-  CURRENT_REFERENCE_ARTICLES,
-  FEATURED_ARTICLE as LIVE_FEATURED_ARTICLE,
-  LATEST_TAX_TALK as LIVE_LATEST_TAX_TALK,
-} from "./current";
-import { LATEST_REFERENCE_ARTICLES } from "./latest";
-import { VERIFIED_REFERENCE_ARTICLES } from "./verified";
-
-export const ALL_REFERENCE_ARTICLES = [
-  ...CURRENT_REFERENCE_ARTICLES,
-  ...LATEST_REFERENCE_ARTICLES,
-  ...VERIFIED_REFERENCE_ARTICLES,
-  ...ALL_HARVEST_ARTICLES,
-].filter(
-  (article, index, array) =>
-    array.findIndex((item) => item.slug === article.slug) === index,
-);
-
-export const CURRENT_FEATURED_ARTICLE = LIVE_FEATURED_ARTICLE;
-export const CURRENT_LATEST_TAX_TALK = LIVE_LATEST_TAX_TALK;
-
-export function getReferenceArticle(slug: string) {
-  return ALL_REFERENCE_ARTICLES.find((article) => article.slug === slug);
-}
+export {
+  ALL_REFERENCE_ARTICLES,
+  CURRENT_FEATURED_ARTICLE,
+  CURRENT_LATEST_TAX_TALK,
+  getReferenceArticle,
+} from "./registry";
