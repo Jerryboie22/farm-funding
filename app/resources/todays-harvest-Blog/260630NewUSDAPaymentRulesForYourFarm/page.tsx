13b75efa19f2f354aea9e8bd0b1c047c57a29d6e
Jerry emeka <jerryemeka22@gmail.com>
2026-08-23 00:49:06 +0100

    Fix USDA article route alias

---
+++ app/resources/todays-harvest-Blog/260630NewUSDAPaymentRulesForYourFarm/page.tsx
@@ -0,0 +1,5 @@
+import { redirect } from "next/navigation";
+
+export default function USDAPaymentRulesRedirectPage() {
+  redirect("/en/resources/todays-harvest-Blog/260630NewUSDAPaymentRulesForYourFarm.html");
+}
