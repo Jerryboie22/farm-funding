296d98139b687e7a7a08082e2e95e43d5256a2d0
Jerry emeka <jerryemeka22@gmail.com>
2026-08-29 05:37:57 +0100

    Redirect legacy tariff relief URL to canonical slug

---
+++ app/resources/todays-harvest-Blog/260701TariffReliefProgramNYProducers.html/page.tsx
@@ -0,0 +1,7 @@
+import { permanentRedirect } from "next/navigation";
+
+export default function TariffReliefLegacyRedirect() {
+  permanentRedirect(
+    "/resources/todays-harvest-Blog/tariff-relief-program-available-for-new-york-producers",
+  );
+}
