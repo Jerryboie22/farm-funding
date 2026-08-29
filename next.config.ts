6552b8c5ca525bc1dfe8c5580ddb2ad9f9a497db
Jerry Emeka <jerryemeka22@gmail.com>
2026-08-29 03:05:14 +0000

    fix: add rewrites for remaining Today's Harvest .html article URLs (404 fix)

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,30 @@ const nextConfig: NextConfig = {
         destination:
           "/en/resources/Industry-Trends-and-Outlooks/Reports/2025NEDairyFarmSummary2026MidYearOutlook.html",
       },
+      {
+        source:
+          "/resources/todays-harvest-Blog/260630NewUSDAPaymentRulesForYourFarm.html",
+        destination:
+          "/en/resources/todays-harvest-Blog/260630NewUSDAPaymentRulesForYourFarm.html",
+      },
+      {
+        source:
+          "/resources/todays-harvest-Blog/260604FarmHighlightsNYSBudget.html",
+        destination:
+          "/en/resources/todays-harvest-Blog/260604FarmHighlightsNYSBudget.html",
+      },
+      {
+        source:
+          "/resources/todays-harvest-Blog/260609CG_BaseAcreReviewForFarmers.html",
+        destination:
+          "/en/resources/todays-harvest-Blog/260609CG_BaseAcreReviewForFarmers.html",
+      },
+      {
+        source:
+          "/resources/todays-harvest-Blog/260701TariffReliefProgramNYProducers.html",
+        destination:
+          "/en/resources/todays-harvest-Blog/260701TariffReliefProgramNYProducers.html",
+      },
     ];
   },
 };
