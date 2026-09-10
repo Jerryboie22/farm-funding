b27f1c7225d8f2a4db69e695d4b6e9e33270de78
Jerry emeka <jerryemeka22@gmail.com>
2026-09-10 14:56:54 -0700

    Fix dairy report route rewrite

--- next.config.ts
+++ next.config.ts
@@ -37,7 +37,7 @@ const nextConfig: NextConfig = {
         source:
           "/resources/todays-harvest-Blog/260630NewUSDAPaymentRulesForYourFarm.html",
         destination:
-          "/en/resources/todays-harvest-Blog/260630NewUSDAPaymentRulesForYourFarm.html",
+          "/en/resources/todays-harvest-Blog/260630NewUSDAPaymentRulesForFarmers.html",
       },
       {
         source:
@@ -62,6 +62,18 @@ const nextConfig: NextConfig = {
           "/resources/Industry-Trends-and-Outlooks/Reports/2606KEP_FuelFertilizerandConflictinMiddleEast",
         destination:
           "/en/resources/Industry-Trends-and-Outlooks/Reports/2606KEP_FuelFertilizerandConflictinMiddleEast.html",
+      },
+      {
+        source:
+          "/resources/Industry-Trends-and-Outlooks/Reports/2605KEP_DairyMidYearTrends",
+        destination:
+          "/en/resources/Industry-Trends-and-Outlooks/Reports/2605KEP_DairyMidYearTrends",
+      },
+      {
+        source:
+          "/resources/Industry-Trends-and-Outlooks/Reports/2605KEP_DairyMidYearTrends.html",
+        destination:
+          "/en/resources/Industry-Trends-and-Outlooks/Reports/2605KEP_DairyMidYearTrends",
       },
     ];
   },
